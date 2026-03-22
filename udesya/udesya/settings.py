634c69800157117c4edfe2f3b8e1e566bd9aa5d8
kauramadhav50 <159455626+kauramadhav50@users.noreply.github.com>
2026-03-22 18:29:55 +0530

    d

--- udesya/udesya/settings.py
+++ udesya/udesya/settings.py
@@ -22,11 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-q9iz4$aio_rht7#)8ed)&(_=$e*h=32-bk3j_p9e6cu@e(o66_'
+# SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
